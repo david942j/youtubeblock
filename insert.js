357ecc16e4a164c52699c397bfe39d2ee552a887
david942j <david942j@gmail.com>
2017-08-29 14:45:10 +0800

    add tube to communication with extension api

--- insert.js
+++ insert.js
@@ -1,6 +1,26 @@
 ;(function() {
+  let storage = chrome.storage.sync
+  function response(data) {
+    window.postMessage({
+      type: 'FromContentScript',
+      data: data
+    }, '*');
+  }
+
+  document.addEventListener('FromPageJS', function(e) {
+    let ar = e.detail.split('.')
+    if(ar[0] == 'storage') {
+      storage.get(ar[1], function(data) {
+        response(data.policy)
+      })
+    }
+  });
+
   var s = document.createElement('script');
   s.src = chrome.extension.getURL('youtubeblock.js')
   s.id = 'youtubeblock'
   ;(document.head || document.documentElement).appendChild(s)
+  s.onload = function() {
+    s.remove();
+  };
 })()
--- youtubeblock.js
+++ youtubeblock.js
@@ -1,8 +1,50 @@
 var Blocker = new function() {
-  let thisObj
+  let thisObj, block = null
   let YT_PLAY = 1
+  let tube = new function() {
+    let response = null
+    window.addEventListener('message', function(event) {
+      if(event.data.type == 'FromContentScript')
+        if(response) response(event.data.data)
+    })
+    return {
+      communiate: function(data, res) {
+        document.dispatchEvent(new CustomEvent('FromPageJS', { detail: data }))
+        response = res
+      }
+    }
+  }
+
+  let Block = function(_policy) {
+    let policy = _policy || {}
+    policy.ids = policy.ids || []
+    policy.substr = policy.substr || []
+    return {
+      isBlock: function(video) {
+        return blockId(video.id) || blockTitle(video.title)
+      }
+    }
+
+    function blockId(videoId) {
+      return policy.ids.includes(videoId)
+    }
+
+    function blockTitle(title) {
+      let ok = true
+      for(var i = 0; i < policy.substr.length; i++)
+        if(title.indexOf(policy.substr[i]) !== -1) {
+          ok = false
+          break
+        }
+      return !ok
+    }
+  }
+
   return thisObj = {
     work: function() {
+      // fetch block pocliy from chrome.storage.sync
+      fetchPolicy()
+      // wait for youbube js binding callbacks
       waitUntilObject(['window'], {
         checker: function(w) {
           return !!searchObject("ytPlayeronStateChangeplayer")
@@ -63,15 +105,28 @@ var Blocker = new function() {
   }
 
   function needSkip() {
-    return blacklist(curVideoId())
+    // blocker not initialized.. data fetching broken?
+    if(!block) return error("Can't initialize blocker")
+    return block.isBlock({ id: curVideoId(), title: curVideoTitle() })
   }
 
   function curVideoId() {
     return ytplayer.config.args.video_id
   }
 
-  function blacklist(videoId) {
-    return ['29tEEaTN_Sc'].includes(videoId)
+  function curVideoTitle() {
+    return ytplayer.config.args.title
+  }
+
+  function fetchPolicy() {
+    tube.communiate('storage.policy', function(policy) {
+      block = new Block(policy)
+    })
+  }
+
+  function error(msg) {
+    console.error(msg)
+    return false
   }
 }
 
